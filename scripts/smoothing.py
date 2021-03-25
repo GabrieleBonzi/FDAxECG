12726f4e85beca5ba3354e7a0cd1f61e5e9bf92d
gabrielebonzi <gabri97.gt@gmail.com>
2021-03-25 19:05:56 +0100

    load ndarray data

--- scripts/smoothing.py
+++ scripts/smoothing.py
@@ -1,5 +1,4 @@
 #%%
-from datetime import datetime
 import matplotlib.pyplot as plt
 import neurokit2 as nk
 import numpy as np
@@ -8,7 +7,6 @@ from scipy import stats
 import seaborn as sns
 import skfda
 from texttable import Texttable
-import wfdb
 import sys
 import random
 
@@ -19,32 +17,34 @@ from fda import *
 #%%
 F = pd.read_csv(data_processed + "female.csv", index_col="ecg_id")
 M = pd.read_csv(data_processed + "male.csv", index_col="ecg_id")
-waves_F = pd.read_csv(data_processed + "waves_F.csv", index_col=0)
-waves_M = pd.read_csv(data_processed + "waves_M.csv", index_col=0)
 
-# %% FEMALE
+# load array
+waves_F = np.load(data_processed + "waves_F.npy")
+waves_M = np.load(data_processed + "waves_M.npy")
 
+data_keys = [
+    "ECG_Start",
+    "ECG_P_Onsets",
+    "ECG_P_Peaks",
+    "ECG_Q_Peaks",
+    "ECG_R_Peaks",
+    "ECG_S_Peaks",
+    "ECG_T_Peaks",
+    "ECG_T_Offsets",
+    "ECG_Stop",
+]
 SAMPLING_RATE = 500
-PATIENT_F = random.choices (np.arange(waves_F.shape[0]),k=5)
 LEAD = 0
 BEAT = 0
-
 ecgM = load_raw_data(M, SAMPLING_RATE, data_raw)
 ecgF = load_raw_data(F, SAMPLING_RATE, data_raw)
 
+# %% Females
+PATIENT_F = random.choices(np.arange(waves_F.shape[0]), k=5)
+
 for p in PATIENT_F:
-    start = list(eval(waves_F.ECG_Start[p]))[BEAT]
-    stop = list(eval(waves_F.ECG_Stop[p]))[BEAT]
-    POnset = list(eval(waves_F.ECG_P_Onsets[p]))[BEAT]
-    PPeak = list(eval(waves_F.ECG_P_Peaks[p]))[BEAT]
-    QPeak = list(eval(waves_F.ECG_Q_Peaks[p]))[BEAT]
-    RPeak = list(eval(waves_F.ECG_R_Peaks[p]))[BEAT]
-    SPeak = list(eval(waves_F.ECG_S_Peaks[p]))[BEAT]
-    TPeak = list(eval(waves_F.ECG_T_Peaks[p]))[BEAT]
-    TOffset = list(eval(waves_F.ECG_T_Offsets[p]))[BEAT]
-    # knots = [start, (start+POnset)*0.5, POnset, (POnset+PPeak)*0.5, PPeak, (PPeak+QPeak)*0.5, QPeak, (QPeak+RPeak)*0.5, RPeak, (RPeak+SPeak)*0.5, SPeak, (SPeak+TPeak)*0.5, TPeak, (TPeak+TOffset)*0.5, TOffset, (TOffset+stop)*0.5, stop]
-    t_knots = [start, POnset, PPeak, QPeak, RPeak, SPeak, TPeak, TOffset, stop]
-    
+    t_knots = [el[BEAT] for el in waves_F[p]]
+
     for d in range(2):
         knots = []
         knots.append(t_knots[0])
@@ -52,13 +52,15 @@ for p in PATIENT_F:
             knots.append((t_knots[i] + t_knots[i + 1]) * 0.5)
             knots.append(t_knots[i + 1])
         t_knots = knots
-        
+
+    start = t_knots[0].astype(int)
+    stop = t_knots[-1].astype(int)
     knots = (np.array(knots) - start) / SAMPLING_RATE
     #
     t_points = np.linspace(0, (stop - start) / SAMPLING_RATE, (stop - start))
     len(t_points)
     # knots = np.linspace(0, (stop - start)/SAMPLING_RATE, 35)
-    
+
     fd_F = skfda.FDataGrid(
         ecgF[p, start:stop, LEAD],
         t_points,
@@ -74,43 +76,32 @@ for p in PATIENT_F:
         n_basis=N_BASIS, domain_range=[0, (stop - start) / SAMPLING_RATE], knots=knots
     )
     basis.plot()
-    
+
     # smoother
     smoother = skfda.preprocessing.smoothing.BasisSmoother(basis, method="cholesky")
-    
+
     # smooothato
     fd_F_smoothed = smoother.fit_transform(fd_F)
-    
-    #figure
+
+    # figure
     fd_axis = []
     fd_s_axis = []
     for b in range(317):
         fd_axis.append(fd_F.data_matrix[0, b, 0])
         fd_s_axis.append(fd_F_smoothed.data_matrix[0, b, 0])
-        
+
     plt.figure()
     plt.plot(fd_axis, label="ECG raw")
     plt.plot(fd_s_axis, label="ECG smoothed")
     plt.legend()
     plt.show()
 
-# %% MALE
-
-PATIENT_M = random.choices (np.arange(waves_M.shape[0]),k=5)
+# %% Males
+PATIENT_M = random.choices(np.arange(waves_M.shape[0]), k=5)
 
 for p in PATIENT_M:
-    start = list(eval(waves_M.ECG_Start[p]))[BEAT]
-    stop = list(eval(waves_M.ECG_Stop[p]))[BEAT]
-    POnset = list(eval(waves_M.ECG_P_Onsets[p]))[BEAT]
-    PPeak = list(eval(waves_M.ECG_P_Peaks[p]))[BEAT]
-    QPeak = list(eval(waves_M.ECG_Q_Peaks[p]))[BEAT]
-    RPeak = list(eval(waves_M.ECG_R_Peaks[p]))[BEAT]
-    SPeak = list(eval(waves_M.ECG_S_Peaks[p]))[BEAT]
-    TPeak = list(eval(waves_M.ECG_T_Peaks[p]))[BEAT]
-    TOffset = list(eval(waves_M.ECG_T_Offsets[p]))[BEAT]
-    # knots = [start, (start+POnset)*0.5, POnset, (POnset+PPeak)*0.5, PPeak, (PPeak+QPeak)*0.5, QPeak, (QPeak+RPeak)*0.5, RPeak, (RPeak+SPeak)*0.5, SPeak, (SPeak+TPeak)*0.5, TPeak, (TPeak+TOffset)*0.5, TOffset, (TOffset+stop)*0.5, stop]
-    t_knots = [start, POnset, PPeak, QPeak, RPeak, SPeak, TPeak, TOffset, stop]
-    
+    t_knots = [el[BEAT] for el in waves_M[p]]
+
     for d in range(2):
         knots = []
         knots.append(t_knots[0])
@@ -118,13 +109,15 @@ for p in PATIENT_M:
             knots.append((t_knots[i] + t_knots[i + 1]) * 0.5)
             knots.append(t_knots[i + 1])
         t_knots = knots
-        
+
+    start = t_knots[0].astype(int)
+    stop = t_knots[-1].astype(int)
     knots = (np.array(knots) - start) / SAMPLING_RATE
     #
     t_points = np.linspace(0, (stop - start) / SAMPLING_RATE, (stop - start))
     len(t_points)
     # knots = np.linspace(0, (stop - start)/SAMPLING_RATE, 35)
-    
+
     fd_M = skfda.FDataGrid(
         ecgM[p, start:stop, LEAD],
         t_points,
@@ -140,29 +133,27 @@ for p in PATIENT_M:
         n_basis=N_BASIS, domain_range=[0, (stop - start) / SAMPLING_RATE], knots=knots
     )
     basis.plot()
-    
+
     # smoother
     smoother = skfda.preprocessing.smoothing.BasisSmoother(basis, method="cholesky")
-    
+
     # smooothato
     fd_M_smoothed = smoother.fit_transform(fd_M)
-    
-    #figure
+
+    # figure
     fd_axis = []
     fd_s_axis = []
     for b in range(317):
         fd_axis.append(fd_M.data_matrix[0, b, 0])
         fd_s_axis.append(fd_M_smoothed.data_matrix[0, b, 0])
-        
+
     plt.figure()
     plt.plot(fd_axis, label="ECG raw")
     plt.plot(fd_s_axis, label="ECG smoothed")
     plt.legend()
     plt.show()
 
-
-######### FIGURE
-# %%
+# %% Figures
 # fd_smoothed.data_matrix.round(2)
 fig1 = fd_F.plot()
 fig2 = fd_F_smoothed.plot()
