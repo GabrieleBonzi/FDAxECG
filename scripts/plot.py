ffd64cb26072e31462f94f1a8b491fbe20790fdc
Riccardo Asnaghi <riccardoasnaghi1997@gmail.com>
2021-07-01 21:54:54 +0200

    CTR/NORM change in figure

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,11 +1,12 @@
 #%% GRAFICI RTSI CONFERENCE
 
+#%%
 t= np.arange(484)/SAMPLING_RATE
 
 fig1 = plt.figure()
 plt.title('Mean',fontsize=16)
 plt.plot(t,t_mean_F_7_12, "r", label="STTC Mean")
-plt.plot(t,t_mean_M_7_12, "b", label="NORM Mean")
+plt.plot(t,t_mean_M_7_12, "b", label="CTR Mean")
 plt.xlim([0,484/SAMPLING_RATE])
 plt.legend()
 
@@ -36,7 +37,7 @@ fpca = FPCA(n_components=n)
 fpca.fit(fd_M_7_12)
 
 fpca.components_.plot(axes=ax1)
-ax1.set_title("NORM Subjects")
+ax1.set_title("CTR Subjects")
 ax1.set_xlabel("Normalized time",fontsize=12)
 ax1.set_ylabel("[a.u.]",fontsize=12)
 
@@ -58,7 +59,7 @@ for i in range(4):
     fpca.components_[i].plot(axes=axs[i], color='b')
     axs[i].set_title("PC" + str(i + 1), fontsize=16)
     axs[i].set_xlim([0,484/SAMPLING_RATE])
-    axs[i].set_ylim([-4,6])
+    axs[i].set_ylim([-5.5,7])
     axs[i].set_ylabel("[a.u.]",fontsize=12)
     
 fpca = FPCA(n_components=n)
@@ -77,7 +78,7 @@ S=D[-29:]
 C=D[:-29]
 
 plt.figure()
-bplot=plt.boxplot((S,C),labels=["STTC","NORM"],patch_artist=True,widths=0.6)
+bplot=plt.boxplot((S,C),labels=["STTC","CTR"],patch_artist=True,widths=0.6)
 
 colors = ['r','b']
 
